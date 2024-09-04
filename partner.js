9b572fff45563410fdbd96c2748f640f880063b3
Kayo Pereira <kayo.richard2000@gmail.com>
2024-09-04 14:43:11 -0300

    adicionado a pagina de parceiro, no qual recebe por paramentro a informação de qual parceiro será exibido

---
+++ partner.js
@@ -0,0 +1,257 @@
+const partnersInfo = {
+  partners: {
+    dell: {
+      logo: "../images/partners/logos_partners/logo_dell.png",
+      buttons: [
+        [
+          "Servidores",
+          "Servidores",
+          "Maximize a Performance e a Segurança do Seu Negócio",
+          "Potência e Confiabilidade são essenciais para o crescimento do seu negócio. Com os Servidores Dell, você garante uma infraestrutura robusta, preparada para as demandas mais exigentes.",
+          "../images/partners/dell/servidor.png",
+        ],
+        [
+          "Storages",
+          "Storages",
+          "Segurança abrangente de dados",
+          "Um amplo armazenamento do mundo com recursos completos de segurança para proteger seus dados contra as ameaças cibernéticas.",
+          "../images/partners/dell/storage.png",
+        ],
+        [
+          "Redes",
+          "Switches",
+          "",
+          "Projetados para fornecer eficiência, com um conjunto de recursos avançados para simplificar o gerenciamento e implementação com confiança em qualquer escala",
+          "../images/partners/dell/switches.png",
+        ],
+        [
+          "Client",
+          "Notebooks, PCs, Periféricos e WorkStations",
+          "",
+          "Os PCs comerciais mais inteligentes e seguros do mundo , com mais de 30 anos de inovação liderada pelo cliente e confiabilidade comprovada. Projetados para o local de trabalho moderno com experiências de usuário confiáveis e gerenciamento simplificado.",
+          "../images/partners/dell/desktops.png",
+        ],
+        [
+          "Hiperconvergência",
+          "Sistemas Hiperconvergentes",
+          "",
+          "Beneficie-se da amplitude do portfólio de HCI da Dell Technologies, que permite opções baseadas na infraestrutura atual, nos modelos operacionais e nos resultados desejados de TI.",
+          "../images/partners/dell/hiperconvergentes.png",
+        ],
+        [
+          "Proteção de Dados/Backup",
+          "Recuperação, backup e armazenamento de dados",
+          "",
+          "Seus dados são inestimáveis. Saiba como fazer backup e restaurar seus dados, software e sistema operacional.",
+          "../images/partners/dell/dados.png",
+        ],
+      ],
+      colors: {
+        banner: "#333333",
+        colorPartner: "#3573d5",
+      },
+    },
+    lenovo: {
+      logo: "../images/partners/logos_partners/logo_lenovo.png",
+      buttons: [
+        [
+          "Servidores",
+          "Servidores",
+          "Impulsione Seu Negócio com os Servidores Lenovo",
+          "Desempenho poderoso, segurança avançada e escalabilidade inteligente para enfrentar qualquer desafio. Lenovo oferece servidores confiáveis, prontos para maximizar a eficiência e a segurança do seu ambiente de TI. Inovação que transforma.",
+          "../images/partners/lenovo/servidores.png",
+        ],
+        ["Client"],
+        ["Storages"],
+        ["Hiperconvergência"],
+        ["Smartphones"],
+      ],
+      colors: {
+        banner: "#333333",
+        colorPartner: "#FF0202",
+      },
+    },
+    cisco: {
+      logo: "../images/partners/logos_partners/logo_cisco.png",
+      buttons: [
+        [
+          "Servidores",
+          "Servidores",
+          "Eleve sua Infraestrutura com os Servidores Cisco",
+          "Desempenho de ponta, segurança robusta e flexibilidade para atender às necessidades do seu negócio. Cisco oferece servidores projetados para garantir eficiência e proteção em ambientes de TI dinâmicos. Conecte-se ao futuro com Cisco.",
+          "../images/partners/cisco/servidores.png",
+        ],
+        ["Hiperconvergência"],
+        ["Redes"],
+        ["Segurança"],
+        ["Observabilidade"],
+        ["Comunicação"],
+      ],
+      colors: {
+        banner: "#333333",
+        colorPartner: "#22AFD1",
+      },
+    },
+    fortinet: {
+      logo: "../images/partners/logos_partners/logo_fortinet.png",
+      buttons: [
+        [
+          "Firewall",
+          "Firewall",
+          "Proteja seu Negócio com Fortinet Firewall",
+          "Segurança de rede líder de mercado, protegendo seus dados contra ameaças cibernéticas. Fortinet oferece firewalls robustos com inteligência integrada para manter sua empresa segura e conectada.",
+          "../images/partners/fortinet/firewall.png",
+        ],
+        ["Redes"],
+        ["Access Points"],
+        ["EDR"],
+        ["SD-WAN"],
+      ],
+      colors: {
+        banner: "#374356",
+        colorPartner: "#DC3024",
+      },
+    },
+    nutanix: {
+      logo: "../images/partners/logos_partners/logo_nutanix.png",
+      buttons: [
+        [
+          "Hiperconvergência",
+          "Hiperconvergência",
+          "Simplifique e Potencialize com Hiperconvergência Nutanix",
+          "Desempenho, agilidade e simplicidade em uma única solução. Com HPE e Nutanix, você transforma sua infraestrutura em uma plataforma hiperconvergente que cresce com seu negócio. Inovação e eficiência sem limites.",
+          "../images/partners/nutanix/hiperconvergencia.png",
+        ],
+        ["Infraestrutura na Nuvem"],
+        ["Virtualização"],
+      ],
+      colors: {
+        banner: "#374356",
+        colorPartner: "#7855FA",
+      },
+    },
+    aws: {
+      logo: "../images/partners/logos_partners/logo_aws.png",
+      buttons: [
+        [
+          "Databases",
+          "Databases",
+          "Potencialize Seus Dados com Databases AWS",
+          "Armazenamento de dados escalável, seguro e altamente disponível. AWS oferece soluções de banco de dados que se adaptam às necessidades do seu negócio, proporcionando alta performance e confiabilidade. AWS - Dados ao seu alcance, sempre.",
+          "../images/partners/aws/databases.png",
+        ],
+        ["VMS"],
+        ["Backup e Recuperação de Dados"],
+      ],
+      colors: {
+        banner: "#374356",
+        colorPartner: "#FF9C08",
+      },
+    },
+    microsoft: {
+      logo: "../images/partners/logos_partners/logo_microsoft.png",
+      buttons: [
+        [
+          "Sistemas",
+          "Sistemas Operacionais",
+          "Transforme seu Negócio com os Sistemas Microsoft",
+          "Software poderoso e integrado para impulsionar a produtividade e a inovação. Microsoft oferece sistemas que otimizam seus processos e aumentam a eficiência, tudo com a segurança que você confia. Microsoft - Tecnologia que faz a diferença.",
+          "../images/partners/microsoft/sistemas.png",
+        ],
+        ["Produtividade"],
+        ["Segurança"],
+        ["Azure +"],
+      ],
+      colors: {
+        banner: "#333333",
+        colorPartner: "#2C3645",
+      },
+    },
+    hpe: {
+      logo: "../images/partners/logos_partners/logo_hpe.png",
+      buttons: [
+        [
+          "Servidores",
+          "Servidores",
+          "Conecte e Potencialize com HPE Aruba Networking",
+          "Desempenho de rede confiável e servidores que suportam as demandas de um ambiente de TI moderno. HPE Aruba oferece soluções que mantêm sua empresa conectada com segurança e eficiência. Aruba - Conectividade de ponta para o seu negócio.",
+          "../images/partners/hpe/servidores.png",
+        ],
+        ["Storages"],
+        ["Redes"],
+      ],
+      colors: {
+        banner: "#374356",
+        colorPartner: "#2C3645",
+      },
+    },
+  },
+};
+
+const urlParams = new URLSearchParams(window.location.search);
+const partner = urlParams.get("partner");
+
+const style = document.createElement("style");
+const cssClass = `
+  .category {
+    color: ${partnersInfo.partners[partner].colors.colorPartner};
+  }
+  .category.active {
+    background-color: ${partnersInfo.partners[partner].colors.colorPartner};
+    color: white;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+style.appendChild(document.createTextNode(cssClass));
+document.head.appendChild(style);
+
+let categorySelector = document.querySelector(".category-selector");
+
+document.querySelector(".partner-logo-banner").style.backgroundColor =
+  partnersInfo.partners[partner].colors.banner;
+document.querySelector(".content-description h1").style.color =
+  partnersInfo.partners[partner].colors.colorPartner;
+
+partnersInfo.partners[partner].buttons.forEach((button, index) => {
+  categorySelector.insertAdjacentHTML(
+    "beforeend",
+    `<div class="category ${index === 0 ? "active" : ""}">${button[0]}</div>`
+  );
+});
+
+let categories = document.querySelectorAll(".category");
+categories.forEach((category, index) => {
+  category.innerText = `${partnersInfo.partners[partner].buttons[index][0]}`;
+});
+
+document.querySelector(".image-container img").src =
+  partnersInfo.partners[`${partner}`].buttons[0][4];
+document.querySelector(".content-description h1").innerText =
+  partnersInfo.partners[`${partner}`].buttons[0][1];
+document.querySelector(".content-description h3").innerText =
+  partnersInfo.partners[`${partner}`].buttons[0][2];
+document.querySelector(".content-description p").innerText =
+  partnersInfo.partners[`${partner}`].buttons[0][3];
+
+const partnerLogoBanner = document.querySelector(".partner-logo-banner img");
+partnerLogoBanner.src = partnersInfo.partners[`${partner}`].logo;
+
+categories.forEach((category, index) => {
+  category.addEventListener("click", (event) => {
+    event.preventDefault();
+    const previousActiveCategory = document.querySelector(".category.active");
+    previousActiveCategory.classList.remove("active");
+    category.classList.add("active");
+
+    document.querySelector(".image-container img").src =
+      partnersInfo.partners[`${partner}`].buttons[index][4];
+    document.querySelector(".content-description h1").innerText =
+      partnersInfo.partners[`${partner}`].buttons[index][1];
+    document.querySelector(".content-description h3").innerText =
+      partnersInfo.partners[`${partner}`].buttons[index][2];
+    document.querySelector(".content-description p").innerText =
+      partnersInfo.partners[`${partner}`].buttons[index][3];
+    console.log(imageContainer);
+  });
+});
